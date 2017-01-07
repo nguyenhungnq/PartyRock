7d37afaf896202b9ed3dd69a5338fa4a1974f5fa
nguyenhungnq <NguyenHung@antonio.local>
2017-01-07 11:05:48 +0700

    Video 1

---
+++ PartyRock/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by NguyenHung on 1/7/17.
+//  Copyright © 2017 NguyenHung. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+  
+
+
+}
+
